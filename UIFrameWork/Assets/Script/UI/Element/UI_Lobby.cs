0270ca5966b7612b87cb352fc1f89e740266138f
junghojoon <junghojoon@studiobside.com>
2022-06-21 17:43:15 +0900

    일단 쓸 때 없는거 지움

--- UIFrameWork/Assets/Script/UI/Element/UI_Lobby.cs
+++ UIFrameWork/Assets/Script/UI/Element/UI_Lobby.cs
@@ -17,18 +17,6 @@ public class UI_Lobby : CJRUIBase
 
     public void OnClick()
     {
-        //UIManager.SendMessageToOpenList(new UIMessage_EnterGameScene(type: MessageType.Type.ClickTab));
         UIManager.Close(this);
-    }
-
-    public override void ReceiveMessage(IUIMessage type)
-    {
-        switch (type.Type)
-        {
-            case MessageType.Type.ClickTab when type is UIMessage_EnterGameScene ClickTempSlot:
-                Debug.Log($"message Type : {ClickTempSlot.Type} _ {this.name}");
-                UIManager.Close(this);
-                break;
-        }
     }
 }
